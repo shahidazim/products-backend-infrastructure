ff1606607ed45ad623271b0b727df0f6dcb60b4d
shahidazim <Shahid.Azim@asos.com>
2022-03-19 17:10:45 +0000

    added function app

---
+++ modules/terraform-azure-function-app/main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.resource_group_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = var.storage_service_plan_name
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "function_app" {
+  name                       = var.function_app_name
+  resource_group_name        = var.resource_group_name
+  location                   = var.resource_group_location
+  app_service_plan_id        = azurerm_app_service_plan.app_service_plan.id
+  storage_account_name       = azurerm_storage_account.storage_account.name
+  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+}
--- modules/terraform-azure-function-app/variables.tf
+++ modules/terraform-azure-function-app/variables.tf
@@ -6,22 +6,6 @@ variable "resource_group_location" {
   description = "Resource group location"
 }
 
-variable "cosmosdb_account_name" {
-  description = "Cosmos DB account name"
-}
-
-variable "cosmosdb_database_name" {
-  description = "Cosmos DB database name"
-}
-
-variable "cosmosdb_database_collection_name" {
-  description = "Cosmos DB collection name"
-}
-
-variable "cosmosdb_database_collection_partition_key" {
-  description = "Cosmos DB collection partition key"
-}
-
 variable "storage_account_name" {
   description = "Storage account name"
 }
